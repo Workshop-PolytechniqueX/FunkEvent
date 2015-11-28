cf0a05e66afe2fad31c92edc66fc42cf15859310
Periwink <charlesbour@gmail.com>
2015-11-28 22:24:02 +0100

    debug seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name, :last_name
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :first_name, :last_name) }
   end
 end
