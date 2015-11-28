388d0a12be5625b011923bd4e4b1a2ba52874e9c
Periwink <charlesbour@gmail.com>
2015-11-28 23:20:58 +0100

    debug auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :likes
   resources :favorites
   resources :images
