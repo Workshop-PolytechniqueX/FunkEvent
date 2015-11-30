1a73dd5886f4aa72746a0cfe2f9afd1616bd2de6
RobinBdt <robin.beaudot@polytechnique.edu>
2015-11-30 16:54:38 +0100

    Ajout autorisations acces

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,5 @@ class Event < ActiveRecord::Base
   has_one :performer
   has_many :images, as: :image_link
 
-  acts_as_mappable :default_units => :kms,
-                   :default_formula => :sphere,
-                   :lat_column_name => :lat,
-                   :lng_column_name => :lng
+  acts_as_mappable :through => :place
 end
