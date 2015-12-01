5bbf4b9523f4e29f7dcb1cfa71083b6e10533a5c
Periwink <charlesbour@gmail.com>
2015-12-01 23:22:09 +0100

    ajout support CORS en rails

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,10 @@ module FunkEvent
         'Access-Control-Allow-Origin' => '*',
         'Access-Control-Request-Method' => '*'
     })
+
+    config.action_dispatch.default_headers = {
+        'Access-Control-Allow-Origin' => 'http://funkevent.herokuapp.com/',
+        'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
   end
 end
