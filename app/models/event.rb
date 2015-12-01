41899dd1582aafd669c53bf3279763ba8052802b
Periwink <charlesbour@gmail.com>
2015-12-01 20:29:12 +0100

    debug

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,8 +5,8 @@ class Event < ActiveRecord::Base
 
 
   scope :cheaper_than, ->(chp) {where("price <= ?", "#{chp}") }
-  scope :category, -> (cat) { joins(:performers).where("performer_category == ?", cat }
-  scope :genre, -> (gnr) { joins(:performers).where("genre == ?", gnr }
+  scope :category, -> (cat) { joins(:performers).where("performer_category == ?", cat) }
+  scope :genre, -> (gnr) { joins(:performers).where("genre == ?", gnr) }
 
 
 
