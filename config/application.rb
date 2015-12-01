0f501691d7e0d2c6068a3baa5cd76cae03c941d2
Periwink <charlesbour@gmail.com>
2015-12-01 23:53:17 +0100

    cors

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,9 @@ module FunkEvent
 
     config.action_dispatch.default_headers = {
         'Access-Control-Allow-Origin' => 'http://funkevent.herokuapp.com/',
-        'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+        'Access-Control-Request-Method' => 'POST, PUT, DELETE, GET, OPTIONS',
+        'Access-Control-Request-Method' => '*',
+        'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     }
 
 
