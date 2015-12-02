b8e5eef6470231b292282dacc22f30ea843b1c38
Periwink <charlesbour@gmail.com>
2015-12-02 23:16:03 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,44 +10,112 @@
 
 
 Place.create(
-    name: "Salle Pleyel",
-    description: "Concert blabla",
-    capacity: 500,
-    website: "http://www.sallepleyel.fr/img/visuel/diaporama/salle_concert_scene.jpg",
-    latitude: 48.8771744,
-    longitude: 2.3013612
+    id: 1,
+    name: "New Morning",
+    description: "Concerts eclectiques de jazz, reggae, rock et musique cubaine dans un lieu legendaire, simple et intimiste.",
+    capacity: 50,
+    url_place: "newmorning.com",
+    adress: "10 Rue des Petites Ecuries, 75010 Paris",
+    latitude: 48.873005,
+    longitude: 2.353232
+)
+
+Image.create(
+    image_link_type: "place",
+    image_link_id: 1,
+    url_image: "https://storage.googleapis.com/static.panoramio.com/photos/medium/89323.jpg"
 )
 
 Place.create(
-    name: "Nouveau Casino",
-    description: "L'Aventura",
-    capacity: 500,
-    website: "http://www.gqmagazine.fr/uploads/images/201421/cc/l_aventura_de_s__bastien_tellier_7651.jpeg",
-    latitude: 48.8658748,
-    longitude: 2.3757088
+    id: 2,
+    name: "Le Comedy Club",
+    description: "Un bar-lounge et une scene accueillant de jeunes talents pour des spectacles d'humour et des jam sessions.",
+    capacity: 200,
+    url_place: "lecomedyclub.com",
+    adress: "42 Boulevard de Bonne Nouvelle, 75010 Paris",
+    latitude: 48.870725,
+    longitude: 2.348469
+)
+
+Image.create(
+    image_link_type: "place",
+    image_link_id: 2,
+    url_image: "https://lh3.googleusercontent.com/-3gcYND-hSyw/UbG1_-MTEVI/AAAAAAAAJKk/gogAp46-GmU/s455-k-no/"
+)
+
+Performer.create(
+    id: 1,
+    name: "Abraxxas",
+    performer_category: "Musician",
+    genre: "Rock",
+    description: "Abraxas est un groupe de ProtoDancepop/Newwave Parisien. http://www.deezer.com/album/9722016
+    'C est le meilleur groupe qui soit' a déclaré quelqu un.",
+    url_performer: "https://www.facebook.com/abraxasofficial"
+)
+
+Image.create(
+         image_link_type: "performer",
+         image_link_id: 1,
+         url_image: "https://scontent-cdg2-1.xx.fbcdn.net/hphotos-ash2/v/l/t1.0-9/10414886_611325479000879_2453807591642275189_n.jpg?oh=dbf3069ee3bef259ffef2c5d03eafb06&oe=56E022C2"
 )
 
 Event.create(
-    name: "Soiree Jazz",
-    place_id: 1,
-    performer_id: 1,
-    description: "Soiree Jazz de 19h à 23h avec des classiques de Louis Armstrong!",
-    date: DateTime.now,
-    price: 0
+   id: 1,
+   name: "Soiree Rock de folie au New Morning",
+   place_id: 1,
+   performer_id: 1,
+   description: "Le jeune groupe de rock etudiant Abraxas va venir enflammer la scene pour une soiree qui va surement rester dans les annales!",
+   date: DateTime.new(2015, 12, 3),
+   price: 5
 )
 
-Performer.create(
-    name: "Celine Dion",
-    performer_category: "Musicien",
-    genre: "Chanson francaise",
-    description: "Chanteuse canadienne devenue celebre depuis Titanic",
-    url: "celinedion.com"
+Place.create(
+    id: 3,
+    name: "Le PomPom",
+    description: "Bar-club branche du 10eme",
+    capacity: 100,
+    url_place: "http://www.planeteparis.fr/Bar-Le-pompon.html",
+    latitude: 48.873815,
+    longitude: 2.350067
 )
 
-Performer.create(
-    name: "Adele",
-    performer_category: "Musicien",
-    genre: "Rock",
-    description: "Chanteuse de la musique du generique du film Skyfall (James Bond)",
-    url: "adele.com"
+Place.create(
+    id: 4,
+    name: "Le Cinquante",
+    description: "Petit bar dans un esprit annees 1950",
+    capacity: 100,
+    url_place: "https://www.facebook.com/Le-Cinquante-195059667184887/",
+    latitude: 48.871865,
+    longitude: 2.362469
+)
+
+Place.create(
+    id: 5,
+    name: "James Hetfeeld's Pub",
+    description: "Pub irlandais à l'ambiance festive",
+    capacity: 100,
+    url_place: "https://www.facebook.com/JamesHetfeeldsPub",
+    latitude: 48.870864,
+    longitude: 2.345522
+)
+
+Image.create(
+    image_link_type: "place",
+image_link_id: 3,
+    url_image: "http://static1.villaschweppes.com/articles/4/73/4/@/2477-le-bar-du-pompon-1230x680-1.jpg"
+
+)
+
+Image.create(
+    image_link_type: "place",
+image_link_id: 4,
+    url_image: "http://s3-media1.fl.yelpcdn.com/bphoto/MQ7FBY8HOhQ3iqGOA-ptyQ/o.jpg"
+
+)
+
+Image.create(
+    image_link_type: "place",
+image_link_id: 5,
+    url_image: "http://www.lesgrandsboulevards.fr/img/enseignes/WEB_029711-05-13.jpg"
+
 )
